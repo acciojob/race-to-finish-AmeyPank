9d90aa7dd2510a5eae2f8b211eaa4ea4fd0a2bb2
AmeyPank <pankantiameya@.com>
2023-04-12 15:45:40 +0530

    Race to finish

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-window.promises = [];
-
 // Do not change the code above this
 // add your promises to the array `promises`
+window.promises = [
+    new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Promise 1")
+        },1000)
+    }),
+    new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Promise 2")
+        },2000)
+    }),
+    new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Promise 3")
+        },3000)
+    }),
+    new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Promise 4")
+        },4000)
+    }),
+    new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Promise 5")
+        },5000)
+    })
+];
+Promise.any(promises).then((result)=>{
+	document.getElementById("output").innerHTML = result;
+});
